1ea88f2ee8643cff53b633c47c9413f6514f8675
cankatmis <ackatmis@gmail.com>
2022-10-08 14:11:27 +0200

    created the projects.

---
+++ G3N8LE_ADT_2022_23_1/G3N8LE_ADT_2022_23_1.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace G3N8LE_ADT_2022_23_1.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ G3N8LE_ADT_2022_23_1/G3N8LE_ADT_2022_23_1.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace G3N8LE_ADT_2022_23_1.Models
+{
+    public class Class1
+    {
+    }
+}
