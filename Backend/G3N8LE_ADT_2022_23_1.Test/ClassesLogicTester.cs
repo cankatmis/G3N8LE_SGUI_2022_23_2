3c33d14fc2cf4b1615f5a4b0b5552988cbcd33bd
cankatmis <ackatmis@gmail.com>
2022-11-15 22:15:15 +0100

    Create ClassesLogicTester.cs

---
+++ Backend/G3N8LE_ADT_2022_23_1.Test/ClassesLogicTester.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G3N8LE_ADT_2022_23_1.Test
+{
+    internal class ClassesLogicTester
+    {
+    }
+}
