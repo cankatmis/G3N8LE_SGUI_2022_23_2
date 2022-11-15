582b065cecbe8d9f89405b1feb67119aca64f6b5
cankatmis <ackatmis@gmail.com>
2022-11-15 22:05:29 +0100

    Create ReservationsLogic.cs

---
+++ Backend/G3N8LE_ADT_2022_23_1.Logic/ReservationsLogic.cs
@@ -0,0 +1,66 @@
+﻿using G3N8LE_ADT_2022_23_1.Models;
+using G3N8LE_ADT_2022_23_1.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G3N8LE_ADT_2022_23_1.Logic
+{
+    public class ReservationsLogic : IReservationsLogic
+    {
+        protected IReservationsRepository _ReservationsRepository;
+        protected IStudentsRepository _studentsrepo;
+        protected ITeachersRepository _teachersrepo;
+
+        public ReservationsLogic(IReservationsRepository reservationsRepository, IStudentsRepository studentsrepo, ITeachersRepository teachersrepo)
+        {
+            _ReservationsRepository = reservationsRepository;
+            _studentsrepo = studentsrepo;
+            _teachersrepo = teachersrepo;
+        }
+
+        public void UpdateReservationDate(Reservations reser)
+        {
+            this._ReservationsRepository.UpdateDate(reser.Id, reser.DateTime);
+        }
+        public Reservations AddNewReservation(Reservations reser)
+        {
+            if (_studentsrepo.GetOne((int)reser.StudentId) == null || _teachersrepo.GetOne((int)reser.TeacherId) == null)
+            {
+                throw new Exception("Data is invalid.");
+            }
+            else
+            {
+                this._ReservationsRepository.Add(reser);
+                return reser;
+            }
+
+        }
+        public void DeleteReservation(int id)
+        {
+            Reservations ReservationToDelete = this._ReservationsRepository.GetOne(id);
+            if (ReservationToDelete != null)
+            {
+                this._ReservationsRepository.Delete(ReservationToDelete);
+            }
+        }
+        public IEnumerable<Reservations> GetAllReservations()
+        {
+            return this._ReservationsRepository.GetAll();
+        }
+        public Reservations GetReservation(int id)
+        {
+            Reservations ReservationToReturn = this._ReservationsRepository.GetOne(id);
+            if (ReservationToReturn != null)
+            {
+                return ReservationToReturn;
+            }
+            else
+            {
+                throw new Exception("The ID could not found in the Reservations database.");
+            }
+        }
+    }
+}
