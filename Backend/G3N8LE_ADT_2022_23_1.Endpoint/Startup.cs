80e9cf6d96da79cbc4085bbcca33b4033f0bde53
cankatmis <ackatmis@gmail.com>
2022-11-22 15:43:46 +0100

    Update Startup.cs

--- Backend/G3N8LE_ADT_2022_23_1.Endpoint/Startup.cs
+++ Backend/G3N8LE_ADT_2022_23_1.Endpoint/Startup.cs
@@ -1,3 +1,6 @@
+using G3N8LE_ADT_2022_23_1.Data;
+using G3N8LE_ADT_2022_23_1.Logic;
+using G3N8LE_ADT_2022_23_1.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,28 +15,44 @@ namespace G3N8LE_ADT_2022_23_1.Endpoint
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+            services.AddControllers();
+            ////services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            ////options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            services.AddTransient<IStudentsLogic, StudentsLogic>();
+            services.AddTransient<ITeachersLogic, TeachersLogic>();
+            services.AddTransient<IReservationsLogic, ReservationsLogic>();
+            services.AddTransient<IReservationsServicesLogic, ReservationsServicesLogic>();
+            services.AddTransient<IClassesLogic, ClassesLogic>();
+            services.AddTransient<IStudentsRepository, StudentsRepository>();
+            services.AddTransient<ITeachersRepository, TeachersRepository>();
+            services.AddTransient<IReservationsRepository, ReservationsRepository>();
+            services.AddTransient<IReservationsServicesRepository, ReservationsServicesRepository>();
+            services.AddTransient<IClassesRepository, ClassesRepository>();
+            services.AddTransient<XYZDbContext, XYZDbContext>();
+            services.AddSignalR();
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(x => x
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .WithOrigins("http://localhost:23079"));
 
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+                endpoints.MapHub<SignalRHub>("/hub");
             });
         }
     }
