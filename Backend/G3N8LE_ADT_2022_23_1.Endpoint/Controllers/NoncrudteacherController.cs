ebdb8a5c1880a191f0c832788d5a31c372471337
cankatmis <ackatmis@gmail.com>
2022-11-22 16:15:18 +0100

    Create NoncrudteacherController.cs

---
+++ Backend/G3N8LE_ADT_2022_23_1.Endpoint/Controllers/NoncrudteacherController.cs
@@ -0,0 +1,42 @@
+﻿using G3N8LE_ADT_2022_23_1.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace G3N8LE_ADT_2022_23_1.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class NoncrudteacherController : ControllerBase
+    {
+        ITeachersLogic Tl;
+
+        public NoncrudteacherController(ITeachersLogic tl)
+        {
+            Tl = tl;
+        }
+
+
+        // GET: Noncrudteacher/TeachersEarnings
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, int>> TeachersEarnings()
+        {
+            return Tl.TeacherEarnings();
+        }
+
+
+        // GET: Noncrudteacher/MostPaidTeachers
+        [HttpGet]
+        public List<KeyValuePair<string, int>> MostPaidTeachers()
+        {
+            return Tl.MostPaidTeacher();
+        }
+
+
+        // GET: Noncrudteacher/LessPaidTeachers
+        [HttpGet]
+        public List<KeyValuePair<string, int>> LessPaidTeachers()
+        {
+            return Tl.LessPaidTeacher();
+        }
+    }
+}
