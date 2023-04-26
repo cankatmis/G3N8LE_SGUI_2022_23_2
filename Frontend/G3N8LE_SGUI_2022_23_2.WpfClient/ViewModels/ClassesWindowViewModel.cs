51bbbd8c9dc6cecfaeb345c79c772fa23e97cbef
cankatmis <ackatmis@gmail.com>
2023-04-26 21:35:59 +0200

    ClassWindowViewModel added

--- Frontend/G3N8LE_SGUI_2022_23_2.WpfClient/ViewModels/ClassesWindowViewModel.cs
+++ Frontend/G3N8LE_SGUI_2022_23_2.WpfClient/ViewModels/ClassesWindowViewModel.cs
@@ -1,12 +1,113 @@
-﻿using System;
+﻿using G3N8LE_ADT_2022_23_1.Models;
+using G3N8LE_SGUI_2022_23_2.WpfClient.Clients;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace G3N8LE_SGUI_2022_23_2.WpfClient.ViewModels
 {
-    internal class ClassesWindowViewModel
+    class ClassesWindowViewModel : ObservableRecipient
     {
+        private ApiClient _apiClient = new ApiClient();
+
+        public ObservableCollection<Classes> Classes { get; set; }
+        private Classes _selectedClass;
+
+        public Classes SelectedClass
+        {
+            get => _selectedClass;
+            set
+            {
+                SetProperty(ref _selectedClass, value);
+            }
+        }
+        private int _selectedClassIndex;
+
+        public int SelectedClassIndex
+        {
+            get => _selectedClassIndex;
+            set
+            {
+                SetProperty(ref _selectedClassIndex, value);
+            }
+        }
+        public RelayCommand AddClassCommand { get; set; }
+        public RelayCommand EditClassCommand { get; set; }
+        public RelayCommand DeleteClassCommand { get; set; }
+        public ClassesWindowViewModel()
+        {
+            Classes = new ObservableCollection<Classes>();
+
+            _apiClient
+                .GetAsync<List<Classes>>("http://localhost:37793/classes")
+                .ContinueWith((classes) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        classes.Result.ForEach((classs) =>
+                        {
+                            Classes.Add(classs);
+                        });
+                    });
+                });
+
+            AddClassCommand = new RelayCommand(AddClass);
+            EditClassCommand = new RelayCommand(EditClass);
+            DeleteClassCommand = new RelayCommand(DeleteClass);
+
+        }
+        private void AddClass()
+        {
+            Classes n = new Classes
+            {
+                Name = SelectedClass.Name,
+                Price = SelectedClass.Price,             
+                Grading = SelectedClass.Grading
+            };
+
+            _apiClient
+                .PostAsync(n, "http://localhost:37793/classes")
+                .ContinueWith((task) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        Classes.Add(n);
+                    });
+                });
+
+        }
+        private void EditClass()
+        {
+            _apiClient
+                .PutAsync(SelectedClass, "http://localhost:37793/classes")
+                .ContinueWith((task) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        int i = SelectedClassIndex;
+                        Classes a = SelectedClass;
+                        Classes.Remove(SelectedClass);
+                        Classes.Insert(i, a);
+                    });
+                });
+        }
+        private void DeleteClass()
+        {
+            _apiClient
+                .DeleteAsync(SelectedClass.Id, "http://localhost:37793/classes")
+                .ContinueWith((task) =>
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        Classes.Remove(SelectedClass);
+                    });
+                });
+        }
     }
 }
--- Frontend/G3N8LE_SGUI_2022_23_2.WpfClient/ViewModels/MainWindowViewModel.cs
+++ Frontend/G3N8LE_SGUI_2022_23_2.WpfClient/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,35 @@ using System.Threading.Tasks;
 
 namespace G3N8LE_SGUI_2022_23_2.WpfClient.ViewModels
 {
-    internal class MainWindowViewModel
+    public class MainWindowViewModel : ObservableRecipient
     {
+        public RelayCommand ManageTeachersCommand { get; set; }
+        public RelayCommand ManageStudentsCommand { get; set; }
+        public RelayCommand ManageReservationsCommand { get; set; }
+        public RelayCommand ManageClassesCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ManageTeachersCommand = new RelayCommand(OpenTeachersWindow);
+            ManageStudentsCommand = new RelayCommand(OpenStudentsWindow);
+            ManageReservationsCommand = new RelayCommand(OpenReservationsWindow);
+            ManageClassesCommand = new RelayCommand(OpenClassesWindow);
+        }
+        private void OpenClassesWindow()
+        {
+            new ClassesWindow().Show();
+        }
+        private void OpenTeachersWindow()
+        {
+            new TeachersWindow().Show();
+        }
+        private void OpenStudentsWindow()
+        {
+            new StudentsWindow().Show();
+        }
+        private void OpenReservationsWindow()
+        {
+            new ReservationsWindow().Show();
+        }
     }
 }
